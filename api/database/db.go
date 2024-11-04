ad083feee5bfc51987fa57b3d84600f7b579d728
AmiltonCabral <amilton.cabral@splab.ufcg.edu.br>
2024-11-04 18:40:36 -0300

    docker file to the api

--- api/database/db.go
+++ api/database/db.go
@@ -4,25 +4,42 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "password"
-	dbname   = "youfood"
-)
-
 func OpenConn() *sql.DB {
-	connInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+	host := os.Getenv("DB_HOST")
+	if host == "" {
+		host = "localhost"
+	}
+
+	port := os.Getenv("DB_PORT")
+	if port == "" {
+		port = "5432"
+	}
+
+	user := os.Getenv("DB_USER")
+	if user == "" {
+		user = "postgres"
+	}
+
+	password := os.Getenv("DB_PASSWORD")
+	if password == "" {
+		password = "password"
+	}
+
+	dbname := os.Getenv("DB_NAME")
+	if dbname == "" {
+		dbname = "youfood"
+	}
+
+	connInfo := fmt.Sprintf("host=%s port=%s user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
 
 	db, err := sql.Open("postgres", connInfo)
-
 	if err != nil {
 		log.Fatal(err)
 	}
--- api/go.mod
+++ api/go.mod
@@ -1,9 +1,8 @@
 module github.com/amiltoncabral/youFood
 
-go 1.23.2
+go 1.23
 
 require (
-	github.com/gorilla/mux v1.8.1
 	github.com/lib/pq v1.10.9
 	golang.org/x/exp v0.0.0-20241009180824-f66d83c29e7c
 )
