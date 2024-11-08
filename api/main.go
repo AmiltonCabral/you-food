1b9020aec451617c8d6cddedb00470c88d4be789
AmiltonCabral <amilton.cabral@splab.ufcg.edu.br>
2024-11-08 08:32:32 -0300

    add secret to database and create cruster to redis

--- api/main.go
+++ api/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"github.com/amiltoncabral/youFood/database"
+	"github.com/amiltoncabral/youFood/redis"
 	"github.com/amiltoncabral/youFood/routes"
 )
 
 func main() {
 	db := database.OpenConn()
+	rc := redis.OpenConn()
 	routes.HandleRequest(db)
 	database.CloseConn(db)
+	redis.CloseConn(rc)
 }
---
+++ api/redis/redis.go
@@ -0,0 +1,57 @@
+package redis
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func OpenConn() *redis.Client {
+	var err error
+
+	password := "" // default, no password set
+	if envPass := os.Getenv("REDIS_PASSWORD"); envPass != "" {
+		password = envPass
+	}
+
+	dbNum := 0 // default DB
+	if db := os.Getenv("REDIS_DB"); db != "" {
+		dbNum, err = strconv.Atoi(db)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	protocolNum := 2 // default connection protocol
+	if protocol := os.Getenv("REDIS_PROTOCOL"); protocol != "" {
+		protocolNum, err = strconv.Atoi(protocol)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_ADDR"),
+		Password: password,
+		DB:       dbNum,
+		Protocol: protocolNum,
+	})
+
+	ctx := context.Background()
+	pong, err := client.Ping(ctx).Result()
+	if err != nil {
+		log.Fatalf("Failed to connect redis: %v", err)
+	}
+
+	fmt.Printf("Successfully connected to redis: %s\n", pong)
+
+	return client
+}
+
+func CloseConn(redisClient *redis.Client) {
+	defer redisClient.Close()
+}
